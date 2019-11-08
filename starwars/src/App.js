b0c22497f6ddf1b2ca860796ed73725c451cdfbe
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-08 12:33:05 -0500

    [Add] Added react-normalize package to App

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import './App.css';
+import Normalize from 'react-normalize';
 
 import CharactersContainer from './components/CharactersContainer/CharactersContainer';
 const App = () => {
@@ -23,6 +23,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <Normalize />
       <h1 className="Header">React Wars</h1>
       <CharactersContainer characters={characters} />
     </div>
