82d5770431c95ca0a19377ab34826d040f4bdc94
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-08 11:25:26 -0500

    [Add] Added useState & useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,12 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() =>{
+    
+  }, []);
+
 
   return (
     <div className="App">
